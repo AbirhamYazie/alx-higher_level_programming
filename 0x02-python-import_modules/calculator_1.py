0fee9bccb3ac03c7f0243caa1592b45134b0e5ae
AbirhamYazie <abirhamyazie24@gmail.com>
2022-06-02 14:38:15 -0700

    import_module

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function.
+    Args:
+        a: first intiger
+        b: second intiger
+    Returns:
+          The return value. a + b
+    """
+    return (a + b)
+
+def sup(a, b):
+    """My subtraction function.
+    Args:
+        a: first intiger
+        b: second intiger
+    Returns:
+          The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function.
+    Args:      
+       a: first intiger
+       b: second intiger
+    Return:
+        The return value. a * b 
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function.
+    Args:
+        a: first intiger
+        b: second intiger
+    Return:
+        The return value. a / b
+    """
+    return int(a / b)
+
