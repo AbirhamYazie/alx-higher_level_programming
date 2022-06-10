e513cbc47978240f9a2be40337ad06a22ceb95c7
AbirhamYazie <abirhamyazie24@gmail.com>
2022-06-10 08:33:29 -0700

    fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     mat = sorted(a_dictionary)
     for item in mat:
-        print('{}: {}'.format(item, a_dictionary[item]))
+        return ('{}: {}'.format(item, a_dictionary[item]))
 
 
