7ade9fe69bd34d04ee97ea26c1adf7b47f304584
AbirhamYazie <abirhamyazie24@gmail.com>
2022-06-10 01:13:18 -0700

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     U_matrix = []
     for row in matrix:
-        U_matrix.append(list(map(lambda e : e**2, row)))
+        U_matrix.append(list(map(lambda e: e**2, row)))
     return (U_matrix)
 
