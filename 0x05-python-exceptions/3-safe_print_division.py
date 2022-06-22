8ddc75e8e9233f4ccaa36788908ec40dfb8050a6
AbirhamYazie <99495640+AbirhamYazie@users.noreply.github.com>
2022-06-23 00:23:21 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         res = (a/b)
-        return res
     except:
         res = None
     finally:
         print('Inside result:{:}'.format(res))
+    return res
