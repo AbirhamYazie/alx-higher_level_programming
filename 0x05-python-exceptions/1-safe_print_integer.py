465c471333a3490430db25d410c3e76a4d17e079
AbirhamYazie <99495640+AbirhamYazie@users.noreply.github.com>
2022-06-23 00:16:15 +0300

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value == int(value):
-            print('{:d}'.format(value))
-            return True
-        else:
-            return False
+        print('{:d}'.format(value))
+        return True
     except ValueError:
         return False
