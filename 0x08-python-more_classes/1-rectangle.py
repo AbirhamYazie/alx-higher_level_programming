882b9b7b282c80ca0cdf12346880f203fc87c82b
AbirhamYazie <abirhamyazie24@gmail.com>
2022-06-30 12:37:56 -0700

    more on class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle:
             height(int): rectangle height.
 
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
     @property
     def height(self):
         """
