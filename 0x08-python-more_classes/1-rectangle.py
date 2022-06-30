6467732864c43661742ad0b373e7886955e42ee1
AbirhamYazie <99495640+AbirhamYazie@users.noreply.github.com>
2022-06-30 23:39:01 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """Real definition of a rectangle"""
 
+
 class Rectangle:
     """Defines the implementation of a rectangle"""
-    
+
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-    
-    @propert
+        self.__width = width
+        self.__height = height
+
+    @property
     def width(self):
         """Property retriever, for retreiving"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-    """Property setter, for setting"""                                              if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-      if value < 0:
-          raise ValueError("width must be >= 0")
-      self.__width = value
-    
+        """Property setter, for setting"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     @property
     def height(self):
-    """Property retriever, for retreiving
-    the rectangle height
-    """
-    return self.__height
-    
+        """Property retriever, for retreiving
+        the rectangle height
+        """
+        return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
